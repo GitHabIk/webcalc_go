6a4aacf3b8d5cb348a466353449cb3bc4b0c076f
GitHabIk <rodypro2013@gmail.com>
2024-12-18 21:22:37 +0300

    Поправил errors.go

--- pkg/calculation/errors.go
+++ pkg/calculation/errors.go
@@ -2,8 +2,5 @@ package calculation
 
 import "errors"
 
-// Ошибка для деления на ноль
 var ErrDivideByZero = errors.New("divide by 0")
-
-// Ошибка для недопустимого выражения
 var ErrInvalidExpression = errors.New("expression is not valid")
